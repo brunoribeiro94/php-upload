00c9914d429bebd734b51ae99e223e9b80d54147
Bruno Ribeiro <offboard@users.noreply.github.com>
2014-12-12 18:57:39 -0300

    initial commit
    first mody

--- example/upload.php
+++ example/upload.php
@@ -1,3 +1,11 @@
-<?phpp
+<?php
 
-// finish
+$upload = new Upload('img');
+$upload
+        ->file_name('uploaded')
+        ->upload_to('/home/user/files/')
+        ->run();
+
+if (!$upload->was_uploaded) {
+    die('error : ' . $upload->error);
+}
