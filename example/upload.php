86806db870709931617f903e92c5ddbef34aadb9
Bruno Ribeiro <offboard@users.noreply.github.com>
2014-12-14 16:19:45 -0300

    message successfully

--- example/upload.php
+++ example/upload.php
@@ -1,11 +1,14 @@
 <?php
 
+include("./Lib/Upload.php");
+
 $upload = new Upload('img');
 $upload
         ->file_name('uploaded')
         ->upload_to('upload/')
         ->run();
-
 if (!$upload->was_uploaded) {
     die('error : ' . $upload->error);
+} else {
+    echo 'image sent successfully !';
 }
