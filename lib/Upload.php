a1acbf8be3211c6b3608309b94f311ee21c1558c
Bruno Ribeiro <offboard@users.noreply.github.com>
2014-12-14 16:20:47 -0300

    Fix upload file
    validations permissions.

--- lib/Upload.php
+++ lib/Upload.php
@@ -30,16 +30,36 @@ class Upload {
     private $file_src_name_ext;
 
     /**
-     * File size
-     * @var int 
+     * Uploaded file size, in bytes
+     *
+     * @access private
+     * @var double
      */
     private $file_src_size;
+
+    /**
+     * Uploaded file size, in bytes
+     *
+     * @access private
+     * @var array
+     */
+    private $file_src_errors = array();
 
     /**
      * File source temp
      * @var string 
      */
     private $file_src_temp;
+
+    /**
+     * Set this variable to change the maximum size in bytes for an uploaded file
+     *
+     * Default value is the value <i>upload_max_filesize</i> from php.ini
+     *
+     * @access private
+     * @var double
+     */
+    private $get_file_max_size = 8000000;
 
     /**
      * 
@@ -50,15 +70,18 @@ class Upload {
         $file = $_FILES[$img];
 
         if (!isset($file)) {
-            $this->error = 'image field not loaded.';
+            $this->error = 'image can not be loaded, Please check the input name if it is equal to the constructor parameter of the class or check for the tag in the form enctype="multipart/form-data"';
             $this->was_uploaded = false;
             return false;
         }
+
         // extract info from file uploaded
         $this->file_src_name = $file["name"];
         $this->file_src_temp = $file["tmp_name"];
         $this->file_src_size = $file["size"];
+        $this->file_src_errors = $file['error'];
         $this->file_src_name_ext = pathinfo($file["name"], PATHINFO_EXTENSION);
+        $this->was_uploaded = true;
         return true;
     }
 
@@ -67,10 +90,15 @@ class Upload {
      * @return boolean
      */
     public function run() {
+
+        $path = $this->upload_to . $this->file_src_name;
+
+        $this->check_size_max();
+        $this->PathValid();
+        $this->check_exist($path);
+
         if ($this->was_uploaded) {
-            self::check_size_max();
-            self::check_exist();
-            if (move_uploaded_file($this->file_src_temp, $this->upload_to . $this->file_src_name)) {
+            if (move_uploaded_file($this->file_src_temp, $path)) {
                 return true;
             } else {
                 $this->was_uploaded = false;
@@ -81,26 +109,42 @@ class Upload {
     }
 
     /**
+     * which the permission of the directory
+     * @return boolean
+     */
+    private function PathValid() {
+        clearstatcache();
+        $permission = substr(sprintf('%o', fileperms($this->upload_to)), -4);
+
+        if ($permission !== '0777') {
+            $this->was_uploaded = false;
+            $this->error = 'Permission denied, permission required 0777 to the directory, current permission is ' . $permission;
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * Check exist file
      * @return boolean
      */
-    private static function check_exist() {
-        if (file_exists($this->upload_to . basename($this->file_src_name))) {
+    private function check_exist($path) {
+        if (file_exists($path)) {
             $this->was_uploaded = false;
             $this->error = $this->file_src_name . ' already exists. Please change the file name';
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
     /**
      * Checks the maximum file size allowed
      * @return boolean
      */
-    private static function check_size_max() {
-        if ($this->file_src_name > $this->file_max_size) {
+    private function check_size_max() {
+        if ($this->file_src_size > $this->get_file_max_size) {
+            $this->error = 'File too big Original Size : ' . $this->file_src_size . ' File size limit : ' . $this->get_file_max_size;
             $this->was_uploaded = false;
-            $this->error = 'File too big';
             return false;
         }
         return true;
@@ -117,12 +161,12 @@ class Upload {
     }
 
     /**
-     * File max size allowed
-     * @param int $size Size in MB
+     * Define file max size allowed
+     * @param int $size Size in Bytes
      * @return \Upload
      */
-    public function file_max_size($size = 8) {
-        $this->file_max_size = (int) $size;
+    public function file_max_size($size) {
+        $this->get_file_max_size = (int) $size;
         return $this;
     }
 
